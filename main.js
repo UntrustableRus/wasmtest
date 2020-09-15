392c0fa58a682baa424d63420980fb9f39aa1163
Rus <46432997+UntrustableRus@users.noreply.github.com>
2020-09-15 18:50:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
-const { main } = wasm_bindgen;
 
-wasm_bindgen('../out/main_bg.wasm').then(function(){
-main();
-}).catch(console.error);
+                (function() {
+                    var wasm;
+                    const __exports = {};
+                    
+
+let cachedDecoder = new TextDecoder('utf-8');
+
+let cachegetUint8Memory = null;
+function getUint8Memory() {
+    if (cachegetUint8Memory === null ||
+        cachegetUint8Memory.buffer !== wasm.memory.buffer)
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    return cachegetUint8Memory;
+}
+
+function getStringFromWasm(ptr, len) {
+    return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+}
+
+__exports.__wbg_f_eval_eval_n = function(arg0, arg1) {
+    let varg0 = getStringFromWasm(arg0, arg1);
+    eval(varg0);
+};
+
+__exports.main = function() {
+    return wasm.main();
+};
+
+                    function init(wasm_path) {
+                        return fetch(wasm_path)
+                            .then(response => response.arrayBuffer())
+                            .then(buffer => WebAssembly.instantiate(buffer, { './rustc_h_e6kikveski': __exports }))
+                            .then(({instance}) => {
+                                wasm = init.wasm = instance.exports;
+                                return;
+                            });
+                    };
+                    self.wasm_bindgen = Object.assign(init, __exports);
+                })();
+            
